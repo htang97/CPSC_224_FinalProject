f0e7f3a0bdeeab9ef0a3d1fd6dc49d28289c3236
htang97 <33075945+htang97@users.noreply.github.com>
2019-04-22 14:30:27 -0700

    Update HelpFrame.java

--- HelpFrame.java
+++ HelpFrame.java
@@ -1,5 +1,14 @@
 // Get Help
 import javax.swing.*;
+
 public class HelpFrame extends JFrame {
+	private int WIN_WIDTH = 300;
+	private int WIN_HEIGHT = 500;
 	
+	public HelpFrame() {
+		setVisible(true);
+		setTitle("Helper");
+		setSize(WIN_WIDTH, WIN_HEIGHT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
