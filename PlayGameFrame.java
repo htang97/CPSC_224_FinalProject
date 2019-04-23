360148769341285223f80380c5f7f4acc3fd5a9f
michael-hepburn <mhepburn@zagmail.gonzaga.edu>
2019-04-23 14:30:21 -0700

    Game itself fully set up

--- PlayGameFrame.java
+++ PlayGameFrame.java
@@ -25,6 +25,10 @@ public class PlayGameFrame extends JFrame{
    private boolean[][]cooperate;//this array stores player 1's response at [0][0-29]
    //Player 2 is [1][0-29]
    private int round;
+   private boolean player1Ready;
+   private boolean player1Choice;
+   private boolean player2Ready;
+   private boolean player2Choice;
 
 	PlayGameFrame() {
 		setVisible(true);
@@ -32,7 +36,12 @@ public class PlayGameFrame extends JFrame{
 		setSize(width, height);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+      cooperate = new boolean[2][30];
       round = 1;
+      player1Ready = false;
+      player1Choice = false;
+      player2Ready = false;
+      player2Choice = false;
 
 		gamePanel = new JPanel();
 		gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.X_AXIS));
@@ -45,7 +54,7 @@ public class PlayGameFrame extends JFrame{
 		gamePanel.add(Box.createRigidArea(new Dimension(40, 10)));
 		notcoButton1 = new JButton("X");
 		gamePanel.add(notcoButton1);
-		label2 = new JLabel("No Trust");
+		label2 = new JLabel("Defect");
 		gamePanel.add(label2);
 		gamePanel.add(Box.createRigidArea(new Dimension(150, 10)));
 		notcoButton2 = new JButton("O");
@@ -55,7 +64,7 @@ public class PlayGameFrame extends JFrame{
 		gamePanel.add(Box.createRigidArea(new Dimension(40, 10)));
 		coButton2 = new JButton("X");
 		gamePanel.add(coButton2);
-		label2 = new JLabel("No Trust");
+		label2 = new JLabel("Defect");
 		gamePanel.add(label2);
 		add(gamePanel);
 
@@ -65,32 +74,89 @@ public class PlayGameFrame extends JFrame{
 		notcoButton2.addActionListener(new ButtonListener2());
 	}
 
+   //Returns the final score for a player
+   //Should be given a 1 or a 2 for the int player
+   private int calculateFinalScore(int player){
+      int total = 0;
+      int self = player - 1;
+      int other;
+      if(player == 1){
+         other = 1;
+      } else {
+         other = 0;
+      }
+
+      for(int i = 0; i < 29; i++){
+         if(cooperate[self][i]){//if the player chose to cooperate
+            if(cooperate[other][i]){//if the opponent also cooperated
+               total += 3;//3 points if both cooperated
+            } else {//if the opponent defected
+               total += 0;//0 points if player was the sucker
+            }
+         } else {//if the player chose not to cooperate
+            if(cooperate[other][i]){//if the opponent cooperated
+               total += 4;//4 points if the opponent is a sucker
+            } else {//if the opponent also defected
+               total += 1;//1 points if both players defected
+            }
+         }
+      }
+      return total;
+   }
+
 	private class ButtonListener1 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == coButton1) {
-
+            player1Ready = true;
+            player1Choice = true;
+            nextRound();
 
 
 			}else if(e.getSource() == notcoButton1) {
-
+            player1Ready = true;
+            player1Choice = false;
+            nextRound();
 
 			}
 		}
 	}
+
 	private class ButtonListener2 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			String actionCommand = e.getActionCommand();
 			//show another player's chose
-			if(e.getSource() == coButton1) {
+			if(e.getSource() == coButton2) {
+            player2Ready = true;
+            player2Choice = true;
+            nextRound();
 
-
-
-			}else if(e.getSource() == notcoButton1) {
-
+			}else if(e.getSource() == notcoButton2) {
+            player2Ready = true;
+            player2Choice = false;
+            nextRound();
 
 			}
 		}
 	}
+
+   //Will move to next round if both players have inputted a move for this round.
+   //Will not advance round if one player is not ready
+   private void nextRound(){
+      if(player1Ready && player2Ready){
+         cooperate[0][round] = player1Choice;
+         cooperate[1][round] = player2Choice;
+         round++;
+         player1Ready = false;
+         player1Choice = false;
+         player2Ready = false;
+         player2Choice = false;
+
+         if(round > 30){
+            //This is where the results panel comes up once the game is over
+         }
+      }
+   }
+
 	public static void main(String [] args) {
 		new PlayGameFrame();
 	}
