975f7ee7fe997b0e68c48b4c203c35d7ae6e1138
michael-hepburn <mhepburn@zagmail.gonzaga.edu>
2019-05-07 17:54:41 -0700

    TournamentFrame work

--- TournamentFrame.java
+++ TournamentFrame.java
@@ -1,5 +1,33 @@
 // Customize a Game
+import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+
 public class TournamentFrame extends JFrame {
+   private int width = 900;
+	private int height = 600;
+   private int numOfRounds;
+   private int numOfPlayers;
+   private int numOfStrats;
+   private Strategy[] players;
+
+	private JPanel gamePanel;//The panel the game is played on
+
+   PlayGameFrame() {//Constructor
+		setVisible(true);
+		setTitle("1v1 Game");
+		setSize(width, height);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+   }
+
+   private int calculateAverage(String name){
+
+   }
+
+   private int calculateTotalPoints(){
+
+   }
+
 
 }
